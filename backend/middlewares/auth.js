a14bce20ae9a46c6890eef1a19e93633294db7eb
rizwanali02 <rizwanali02hindoli@gmail.com>
2024-03-26 15:32:54 +0530

    test 3

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,28 +4,36 @@ import { ErrorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 
-const isAuthenticated = asyncHandler(async (req, res, next) => {
-  const { token } = req.cookies;
+const isAuthenticated = async (req, res, next) => {
+  // Check if token is in cookies or headers
+  const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+ 
   if (!token) {
-    return next(new ErrorHandler("User is not authenticated", 400));
+     return res.status(400).json({ success: false, message: "User is not authenticated" });
   }
+ 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.user = await User.findById(decoded.id);
-    next();
+     // Verify the token
+     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+     const user = await User.findById(decoded.id);
+     if (!user) {
+       return res.status(400).json({ success: false, message: "User not found" });
+     }
+     req.user = user;
+     next();
   } catch (error) {
-    console.error("Token verification failed:", error);
-    return next(new ErrorHandler("Invalid token", 401));
+     console.error("Token verification failed:", error);
+     return res.status(401).json({ success: false, message: "Invalid token" });
   }
-});
+ };
 
 
 const isAuthorized = (isAdminRequired) => {
   return (req, res, next) => {
     if (isAdminRequired && req.user.isAdmin === true) {
-      next(); // Call next() only if isAdmin is true and admin authorization is required
+      next(); 
     } else {
-      return next(new ErrorHandler(`You are not an Admin`, 403)); 
+      return next(new ErrorHandler(`You are not an Admin`, 403));
     }
   };
 };
